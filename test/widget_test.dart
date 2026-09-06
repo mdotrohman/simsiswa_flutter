25e6a988750e31256a147bc96a9c39d6468fe93e
root <root@localhost.localdomain>
2026-09-06 06:50:08 +0000

    test: widget test ikut alur baru (index portal -> login); v1.26

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -10,9 +10,15 @@ void main() {
     await Session.init();
   });
 
-  testWidgets('App boots to login page', (WidgetTester tester) async {
+  testWidgets('App boots to index portal then goes to login', (WidgetTester tester) async {
     await tester.pumpWidget(const SimSiswaApp());
     await tester.pump();
+
+    expect(find.text('SIM Siswa MTsB.U'), findsOneWidget);
+    expect(find.text('Masuk / Daftar'), findsOneWidget);
+
+    await tester.tap(find.text('Masuk / Daftar'));
+    await tester.pumpAndSettle();
 
     expect(find.text('SIM Siswa MTsBU'), findsOneWidget);
   });
